46f4f54bdab49997a0fe4d6063defb510dbdba46
prokop <inkaenfu69@gmail.com>
2024-03-11 08:30:44 +0100

    Added method for watering flowers Added needWater and Watered in class Flower Addede harvesting method

--- src/Farmar.java
+++ src/Farmar.java
@@ -2,14 +2,20 @@ import java.util.ArrayList;
 
 public class Farmar {
     private ArrayList<Flower> flowers;
+    private double obsazenyPozemek;
+    private static final int MAX_ROSTLINY = 5;
+    private static final double MAX_POZEMEK = 100.0;
+
 
     public Farmar() {
         this.flowers = new ArrayList<>();
+        this.obsazenyPozemek = 0.0;
     }
 
     public void plantFlower(Flower flower) {
         flowers.add(flower);
     }
+
     public void waterFlower() {
         for (Flower flower : flowers) {
             if (flower.needWater) {
@@ -20,11 +26,19 @@ public class Farmar {
             }
         }
     }
-    public void harvestFlowers() {
-        for (Flower flower : flowers) {
+
+    public void harvestFlowers(Flower flower) {
+        if (flowers.size() < MAX_ROSTLINY && (obsazenyPozemek + flower.neededArea) <= MAX_POZEMEK) {
+            flowers.add(flower);
+            obsazenyPozemek += flower.neededArea;
+            System.out.println(flower.getName() + "Planted");
+        } else {
+            System.out.println("Nelze zasadit");
+
         }
     }
 }
+
 
 
 
