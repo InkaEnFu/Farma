cc95e6bb767d0357d5aa1c27eb2259d88780c4f6
prokop <inkaenfu69@gmail.com>
2024-03-11 08:19:19 +0100

    Added method for watering flowers Added needWater and Watered in class Flower

--- src/Farmar.java
+++ src/Farmar.java
@@ -1,15 +1,26 @@
 import java.util.ArrayList;
 
-public class Farmar extends Flower {
+public class Farmar {
     private ArrayList<Flower> flowers;
 
     public Farmar() {
         this.flowers = new ArrayList<>();
     }
 
-    public void zasaditKvetiny(Flower flower) {
+    public void plantFlower(Flower flower) {
         flowers.add(flower);
-
+    }
+    public void waterFlower() {
+        for (Flower flower : flowers) {
+            if (flower.needWater) {
+                System.out.println("Watering " + flower.name + ".");
+                flower.setWatered(true);
+            } else {
+                System.out.println(flower.name + " no need to water anymore");
+            }
+        }
     }
 
 }
+
+
--- src/Flower.java
+++ src/Flower.java
@@ -1,11 +1,32 @@
-import java.util.ArrayList;
-
 public abstract class Flower {
 
     protected String name;
     protected double price;
     protected double neededArea;
     protected double chanceOfGrowth;
+    protected boolean needWater;
+    protected boolean watered;
+
+    public Flower(String name, double price, double neededArea, double chanceOfGrowth, boolean potrebujesZavlazit, boolean watered) {
+        this.name = name;
+        this.price = price;
+        this.neededArea = neededArea;
+        this.chanceOfGrowth = chanceOfGrowth;
+        this.needWater = potrebujesZavlazit;
+        this.watered = watered;
+    }
+
+    public void setNeedWater(boolean needWater) {
+        this.needWater = needWater;
+    }
+
+    public void setWatered(boolean watered) {
+        this.watered = watered;
+    }
+
+    public String getName() {
+        return name;
+    }
 
     @Override
     public String toString() {
